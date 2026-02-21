7aaea8b1f9fa575ea5d2aa57af7124d282b4a854
MonsterGuti <gogulanovmartin@gmail.com>
2026-02-21 21:58:03 +0200

    Added urls for Rentals.

--- rentals/urls.py
+++ rentals/urls.py
@@ -1,4 +1,12 @@
+from django.urls import path
+
+from rentals.views import RentalListView, RentalDetailView, RentalCreateView, RentalUpdateView, RentalDeleteView
+
 app_name = 'rentals'
 urlpatterns = [
-
+    path('', RentalListView.as_view(), name='list'),
+    path('<int:pk>/', RentalDetailView.as_view(), name='detail'),
+    path('create/', RentalCreateView.as_view(), name='create'),
+    path('<int:pk>/update/', RentalUpdateView.as_view(), name='update'),
+    path('<int:pk>/delete/', RentalDeleteView.as_view(), name='delete'),
 ]
