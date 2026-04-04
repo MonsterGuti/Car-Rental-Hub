a5e65abec7952b1e32d801a833ca0bb935a52a3a
MonsterGuti <gogulanovmartin@gmail.com>
2026-04-04 13:07:02 +0300

    redirect to home page after register.

--- accounts/views.py
+++ accounts/views.py
@@ -1,16 +1,25 @@
+from django.contrib.auth import login
 from django.urls import reverse_lazy
 from django.contrib.auth.views import LoginView, LogoutView
 from django.views.generic import CreateView
 from .forms import RegisterForm
 
+
 class RegisterView(CreateView):
     form_class = RegisterForm
     template_name = 'accounts/register.html'
-    success_url = reverse_lazy('accounts:login')
+    success_url = reverse_lazy('common:home')
+
+    def form_valid(self, form):
+        response = super().form_valid(form)
+        login(self.request, self.object)
+        return response
+
 
 class AppLoginView(LoginView):
     template_name = 'accounts/login.html'
     success_url = reverse_lazy('cars:car-list')
 
+
 class AppLogoutView(LogoutView):
-    success_url = reverse_lazy('common:home')
+    success_url = reverse_lazy('accounts:login')
