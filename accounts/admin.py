4de806654aca9fe6149f8bc6a586a13be3980f87
MonsterGuti <gogulanovmartin@gmail.com>
2026-04-04 12:17:38 +0300

    Registered AppUser in admin.

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import AppUser
 
-# Register your models here.
+@admin.register(AppUser)
+class AppUserAdmin(admin.ModelAdmin):
+    list_display = ('username', 'email', 'is_staff', 'is_owner')
